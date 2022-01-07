9b2db5f8e3328c89e0171dcfaa81087367df6fc2
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-01-07 10:24:58 -0500

    updated code

--- controllers/Home-Routes.js
+++ controllers/Home-Routes.js
@@ -1,37 +1,13 @@
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
+
+
 router.get('/', (req, res) => {
-    Post.findAll({
-            attributes: [
-                'id',
-                'title',
-                'content',
-                'created_at'
-            ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ]
-        })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  res.render('homepage');
 });
+
+module.exports = router;
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
