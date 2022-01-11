774cdd47c4b0871849f22f799155b0946c8520ba
Mona Lisa <holmesmainsend@gmail.com>
2022-01-10 20:18:27 -0500

    more minor README fixes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const colors = require('colors');
 
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
