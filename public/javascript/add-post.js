a7a9b5f5d690c4aee64c17ae0d37afdd280009f0
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-01-12 10:06:30 -0500

    updated code

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -3,15 +3,13 @@ async function newFormHandler(event) {
   
     const title = document.querySelector('input[id="post-title"]').value;
     const post_url = document.querySelector('input[id="post_url"]').value;
-    const image = document.querySelector('input[id="image"]').value;
 
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
         title,
-        post_url,
-        image
+        post_url
       }),
       headers: {
         'Content-Type': 'application/json'
